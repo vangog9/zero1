f46a50861c8b773c388d5dfeb73aaea562ea9299
SN422guest <vangog9@mail.ru>
2021-06-14 00:13:07 +0300

    Calculating $how_many

--- app/Commands/StatsCommand.php
+++ app/Commands/StatsCommand.php
@@ -5,6 +5,7 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Http;
 use LaravelZero\Framework\Commands\Command;
+use Illuminate\Support\Facades\Log;
 
 class StatsCommand extends Command
 {
@@ -31,26 +32,36 @@ class StatsCommand extends Command
     {
         $username = $this->argument('username');
 
-        $search_response = Http::get("https://api.github.com/search/code?q={$username}&access_token=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");
+        $search_response = Http::get("https://api.github.com/search/code?q={$username}&access_token=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");
 
-//        if (!$search_response->ok()) {
-//            $this->warn('Nope!!!!!!!!!!!!!!!!!');
-//            return;
-//        }
-        //      , 'name'=>$name    and name is {$name}
         ['total_count' => $count] = $search_response->json();
-        //$search = $search_response->json();
-        $searchArray = json_decode($search_response, false);
+        if ($count == 0) {
+            $this->warn('EMPTY!!!!!');
+            return;
+        }
+
+        $page_number = ceil($count / 100);
         $this->info("The number of {$username} is {$count} ");
 
-        dd(array_count_values($searchArray));
+        for ($page = 0; $page <= $page_number - 1; $page++) {
+            $current_page = $page + 1;
+            $search_response = Http::get("https://api.github.com/search/code?q={$username}&access_token=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&per_page=100&page={$current_page}");
 
+//            if (!$search_response[$page]->successful()) {
+//                $this->warn('Nope!!!!!!!!!!!!!!!!!');
+//                return;
+//            }
 
+            $searchArray = json_decode($search_response, true);
+            $how_many = 100-(100-$count);
 
-        //$names[] = $searchArray['items']['name'];
-
-        //dd($names);
-
+            for ($item = 0; $item < $how_many; $item++) {
+                $this->info($searchArray['items'][$item]['name']);
+                $this->info('');
+                $this->info($searchArray['items'][$item]['html_url']);
+                $this->info('');
+            }
+        }
     }
 
     /**
@@ -59,7 +70,8 @@ class StatsCommand extends Command
      * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
-    public function schedule(Schedule $schedule): void
+    public
+    function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
     }
