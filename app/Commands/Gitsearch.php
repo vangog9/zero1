db52ed743a701364c31a63814deb278112e9ce90
al-php <e@mail.com>
2021-06-22 13:01:22 +0300

    UpdateCommit

--- app/Commands/Gitsearch.php
+++ app/Commands/Gitsearch.php
@@ -5,24 +5,22 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Http;
 use LaravelZero\Framework\Commands\Command;
-use Illuminate\Support\Facades\Log;
-use Symfony\Component\Console\Input\InputArgument;
 
-class StatsCommand extends Command
+class Gitsearch extends Command
 {
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'stats {username} {token}';
-//
+    protected $signature = 'gitsearch {search_word} {token}';
+
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Retrieve the Ecologi statistics for a user';
+    protected $description = 'Retrieve the GitSearch';
 
     /**
      * Execute the console command.
@@ -31,10 +29,10 @@ class StatsCommand extends Command
      */
     public function handle(): void
     {
-        $username = $this->Argument('username');
+        $search_word = $this->Argument('search_word');
         $token = $this->Argument('token');
 
-        $search_response = Http::get("https://api.github.com/search/code?q={$username}&access_token={$token}");
+        $search_response = Http::get("https://api.github.com/search/code?q={$search_word}&access_token={$token}");
 
         if (!$search_response->successful()) {
             $this->warn('Nope!!!!!!!!!!!!!!!!!');
@@ -49,11 +47,11 @@ class StatsCommand extends Command
         }
 
         $page_number = ceil($count / 100);
-        $this->info("The number of {$username} is {$count} ");
+        $this->info("The number of {$search_word} is {$count} ");
 
         for ($page = 0; $page < $page_number; $page++) {
             $current_page = $page + 1;
-            $search_response = Http::get("https://api.github.com/search/code?q={$username}&access_token={$token}&per_page=100&page={$current_page}");
+            $search_response = Http::get("https://api.github.com/search/code?q={$search_word}&access_token={$token}&per_page=100&page={$current_page}");
 
             $searchArray = json_decode($search_response, true);
 
